7eaf8c2c8c639a3dc85ab4a3a3068e04cb1cd1fa
ChenYingPing <1417alk@sina.com>
2015-10-29 16:47:45 +0800

    Initial Commit

---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Chen on 15/10/29.
+//  Copyright © 2015年 Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
